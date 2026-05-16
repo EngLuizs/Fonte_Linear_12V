cfbfcbc92c585e049c6020834877d7635af4e06e
EngLuizs <eng.luizluz@gmail.com>
2026-05-15 21:23:56 -0300

    Alteração no valor maximo e minimo de sobre corrente dos leds

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -218,8 +218,8 @@
           // ===== POTENCIA =====
             potenciaAtual = tensaoAtual * correnteAtual;
 
-          digitalWrite(PIN_LDGREEN, correnteAtual < 1.5);
-          digitalWrite(PIN_LDRED, correnteAtual >= 1.5);
+          digitalWrite(PIN_LDGREEN, correnteAtual < 1.2);
+          digitalWrite(PIN_LDRED, correnteAtual >= 1.2);
 
         // ================ ATUALIZAÇÃO DO LCD ================
           lcd.setCursor(0, 0);
